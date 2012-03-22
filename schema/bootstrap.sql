473b6f08962d21b01926e35f7aad737e4ee7d861
Rahul Gidwani <rahul.gidwani@gmail.com>
2012-03-21 21:19:16 -0700

    Added mysql connection fixed schema show something on the web

---
+++ schema/bootstrap.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`id`, `username`, `firstname`, `lastname`, `chapter_id`, `last_login`, `password`, `school`, `year`, `active`)
+VALUES(1, 'galengt', 'galen', 'thompson', NULL, '2012-03-21 20:55:18', 'password', 'stanford', 2005, 0);
