df9d6c772bea869918670829fb2edbddb0b48c13
Rahul Gidwani <rahul.gidwani@gmail.com>
2012-03-25 18:56:54 -0700

    Getting rid of useless files

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ exports.index = function(req, res){
   res.render('index', { title: 'Face Aids - Please Login' })
 };
 
+exports.login = function(req, res) {
+    res.
+};
+
 exports.chapters = function(req, res){
 	require('./query.js').makeQuery("Select username from faceaids.user",  function selectCb(err, results, fields) {
 		if (err) {
